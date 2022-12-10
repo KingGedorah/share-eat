d302003252694da68c9519f6448e201bd8aaf836
jason.irvine <jason.irvine@ui.ac.id>
2022-12-10 17:44:30 +0700

    feat: fitur seller

---
+++ lib/form/edit_makanan.dart
@@ -0,0 +1,259 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:share_eat/drawer/drawer.dart';
+import 'package:share_eat/main.dart';
+import 'package:share_eat/page_makanan.dart';
+import 'package:share_eat/model/makanan_model.dart';
+
+class EditMakananFormPage extends StatefulWidget {
+  final Makanan makanan;
+  const EditMakananFormPage({super.key, required this.makanan});
+
+  @override
+  State<EditMakananFormPage> createState() => _EditMakananPageState();
+}
+
+class _Makanan {
+  String name = "";
+  String description = "";
+  int harga = 0;
+  int stok = 0;
+  _Makanan(this.name, this.description, this.harga, this.stok);
+}
+
+class _EditMakananPageState extends State<EditMakananFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _namaMakanan = "";
+  int _harga = 0;
+  String _deskripsi = "";
+  int _stok = 0;
+  String _tempStok = "";
+  String _tempHarga = "";
+
+  void initState() {
+    _namaMakanan = widget.makanan.fields.name;
+    _tempHarga = widget.makanan.fields.harga.toString();
+    _deskripsi = widget.makanan.fields.description;
+    _tempStok = widget.makanan.fields.stok.toString();
+  }
+
+  updateMakanan(request, makanan) async {
+    final response = await request.post(
+        'http://127.0.0.1:8000/seller_menu/update/makanan/${widget.makanan.pk}',
+        {
+          'food_name': makanan.name,
+          'description': makanan.description,
+          'harga': makanan.harga.toString(),
+          'stok': makanan.stok.toString(),
+        });
+    return Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const MakananPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Makanan'),
+      ),
+      drawer: DrawerWidget(),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      // Menggunakan padding sebesar 8 pixels
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        initialValue: widget.makanan.fields.name,
+                        decoration: InputDecoration(
+                          hintText: "Nama makanan",
+
+                          // Menambahkan circular border agar lebih rapi
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        // Menambahkan behavior saat nama diketik
+                        onChanged: (String? value) {
+                          setState(() {
+                            _namaMakanan = value!;
+                          });
+                        },
+                        // Menambahkan behavior saat data disimpan
+                        onSaved: (String? value) {
+                          setState(() {
+                            _namaMakanan = value!;
+                          });
+                        },
+                        // Validator sebagai validasi form
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Nama makanan tidak boleh kosong!';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      // Menggunakan padding sebesar 8 pixels
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        initialValue: widget.makanan.fields.harga.toString(),
+                        decoration: InputDecoration(
+                          hintText: "Harga",
+
+                          // Menambahkan circular border agar lebih rapi
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        // Menambahkan behavior saat nama diketik
+                        onChanged: (String? value) {
+                          setState(() {
+                            _tempHarga = value!;
+                          });
+                        },
+                        // Menambahkan behavior saat data disimpan
+                        onSaved: (String? value) {
+                          setState(() {
+                            _harga = int.parse(value!);
+                          });
+                        },
+                        // Validator sebagai validasi form
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Harga tidak boleh kosong!';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      // Menggunakan padding sebesar 8 pixels
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        initialValue: widget.makanan.fields.description,
+                        decoration: InputDecoration(
+                          hintText: "Deskripsi",
+
+                          // Menambahkan circular border agar lebih rapi
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        // Menambahkan behavior saat nama diketik
+                        onChanged: (String? value) {
+                          setState(() {
+                            _deskripsi = value!;
+                          });
+                        },
+                        // Menambahkan behavior saat data disimpan
+                        onSaved: (String? value) {
+                          setState(() {
+                            _deskripsi = value!;
+                          });
+                        },
+                        // Validator sebagai validasi form
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Deskripsi tidak boleh kosong!';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      // Menggunakan padding sebesar 8 pixels
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        initialValue: widget.makanan.fields.stok.toString(),
+                        decoration: InputDecoration(
+                          hintText: "Stok",
+
+                          // Menambahkan circular border agar lebih rapi
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        // Menambahkan behavior saat nama diketik
+                        onChanged: (String? value) {
+                          setState(() {
+                            _tempStok = value!;
+                          });
+                        },
+
+                        // Menambahkan behavior saat data disimpan
+                        onSaved: (String? value) {
+                          setState(() {
+                            _stok = int.parse(value!);
+                          });
+                        },
+                        // Validator sebagai validasi form
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Stok tidak boleh kosong!';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: TextButton(
+                child: const Text(
+                  "Update Makanan",
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _harga = int.parse(_tempHarga);
+                    _stok = int.parse(_tempStok);
+                    final _makanan =
+                        new _Makanan(_namaMakanan, _deskripsi, _harga, _stok);
+                    updateMakanan(request, _makanan);
+                    // final _budget =
+                    //     new Budget(_judul, _harga, jenisBudget, selectDate);
+                    // MyFormPage.budgetList.add(_budget);
+                  }
+                },
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Back',
+                  style: TextStyle(color: Color(0xffffffff)),
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:share_eat/drawer/drawer.dart';
+import 'package:share_eat/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +14,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+        title: 'Flutter App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter App'),
+        routes: {
+          "/login": (BuildContext context) => const LoginPage(),
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -75,6 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      drawer: DrawerWidget(),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
