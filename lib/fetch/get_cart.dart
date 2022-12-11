ab6bb6ac6d4e04439d2ef5d5722b3dd596f212d4
naufal zhafari <zhafarinaufal@gmail.com>
2022-12-11 11:34:29 +0700

    commit

--- lib/fetch/get_cart.dart
+++ lib/fetch/get_cart.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../model/cart_model.dart';
 
-Future<List<CartUser>> fetchCartUser(user) async {
+Future<List<CartUser>> fetchCartUser(id) async {
   var url = Uri.parse('https://share-eat-d02.up.railway.app/fitur_keranjang/json/');
   var response = await http.get(
     url,
@@ -16,7 +16,7 @@ Future<List<CartUser>> fetchCartUser(user) async {
 
   List<CartUser> listCart = [];
   for(var d in data) {
-    if (d != null && CartUser.fromJson(d).fields.user == user) {
+    if (d != null && CartUser.fromJson(d).fields.keranjang == id) {
       listCart.add(CartUser.fromJson(d));
     }
   }
